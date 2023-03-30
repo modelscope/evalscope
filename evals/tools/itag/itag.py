501b9ced9f88db54fecab60131daec5d63cb0d6d
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-03-30 23:34:49 +0800

    add itag info

--- evals/tools/itag/itag.py
+++ evals/tools/itag/itag.py
@@ -8,10 +8,26 @@ from alpha_data_sdk import models as alphad_model
 
 
 class ItagManager(object):
+    """
+    iTag manager.
 
-    def __init__(self):
-        # self._init_itag_client()
-        pass
+    Args:
+        tenant_id: Tenant id.
+        token: Token.
+        employee_id: Employee id.
+
+    Examples:
+        >>> from evals.tools import ItagManager
+        >>> itag_cfg = dict(tenant_id="xxx", token="xxx", employee_id="xxx")
+        >>> itag_manager = ItagManager(**itag_cfg)
+        >>> itag_manager.create_dataset()
+        >>> itag_manager.create_tag_task()
+        >>> itag_manager.get_tag_task_result()
+
+    """
+
+    def __init__(self, **kwargs):
+        self._init_itag_client(**kwargs)
 
     def _init_itag_client(self, tenant_id, token, employee_id):
         """
@@ -36,6 +52,12 @@ class ItagManager(object):
 
         # todo: TBD ...
 
+    def _task_result_parser(self):
+        """
+        Parse task result.
+        """
+        pass
+
     def create_dataset(self):
         """
         Create a dataset from local file.
@@ -53,3 +75,4 @@ class ItagManager(object):
         Fetch tag task result.
         """
         pass
+
