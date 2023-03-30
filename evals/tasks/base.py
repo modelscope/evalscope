68241d5f371f50522cf3ec24414aa4de7f0cc377
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-03-30 17:04:14 +0800

    add gpt predictor and refine task

--- evals/tasks/base.py
+++ evals/tasks/base.py
@@ -1,17 +1,38 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
 from evals import Eval
+from evals.predictors import Predictor
 
 
 class EvalTask(object):
 
-    def __init__(self, eval_cls: Eval):
+    def __init__(self, predictor: Predictor, eval_cls: Eval, **kwargs):
+        # TODO: task_name to be added in registry
+
+        self._predictor = predictor
         self._eval_cls = eval_cls
+
+        if not isinstance(self._predictor, Predictor):
+            raise TypeError('predictor must be an instance of evals.predictors.Predictor')
 
         if not isinstance(self._eval_cls, Eval):
             raise TypeError('eval_cls must be an instance of evals.Eval')
 
     def run(self):
+
+        # 1. get samples
+
+        # 2. get_model meta info
+
+        # 3. get batches (or add_batches) --P1
+
+        # 4. run inference
+        result_dict = self.run_inference(**input_args)
+
+        # 5. gen report
+
+        # 6. dump result
+
         ...
 
     def get_samples(self):
@@ -23,8 +44,9 @@ class EvalTask(object):
     def get_batches(self):
         ...
 
-    def run_inference(self):
-        ...
+    def run_inference(self, **input_args) -> dict:
+        result_dict = self._predictor(**input_args)
+        return result_dict
 
     def gen_report(self):
         ...
