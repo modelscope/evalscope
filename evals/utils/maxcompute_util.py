646c48dec384e7753c42e64493190417fb5e9571
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-04-01 19:53:05 +0800

    add odps util

---
+++ evals/utils/maxcompute_util.py
@@ -0,0 +1,44 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from odps import ODPS
+import pandas as pd
+
+
+class MaxComputeUtil:
+    """
+    MaxCompute util class.
+
+    Args:
+        access_id: your access id of MaxCompute
+        access_key: access key of MaxCompute
+        project_name: your project name of MaxCompute
+        endpoint: endpoint of MaxCompute
+
+    Attributes:
+        _odps: ODPS object
+
+    """
+
+    def __init__(self, access_id, access_key, project_name, endpoint):
+        self._odps = ODPS(access_id, access_key, project_name, endpoint)
+
+    def get_table(self, table_name):
+        """
+        Get MaxCompute table object.
+        """
+        return self._odps.get_table(table_name)
+
+    def read_data(self, table_name: str, pt_condition: str) -> pd.DataFrame:
+        """
+        Read data from MaxCompute table.
+        :param table_name: table name
+        :param pt_condition: partition condition,
+            Example: pt_condition = 'dt=20230331'
+        :return: pandas dataframe with all data
+        """
+        t = self.get_table(table_name)
+
+        with t.open_reader(partition=pt_condition) as reader:
+            pd_df = reader.to_pandas()
+
+        return pd_df
