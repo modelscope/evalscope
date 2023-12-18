b6a1ec64b85be1e27b763729f90f1474a2d27002
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2023-12-18 17:18:53 +0800

    normalize domain names for mmlu and ceval

--- llmuses/tools/gen_mmlu_subject_mapping.py
+++ llmuses/tools/gen_mmlu_subject_mapping.py
@@ -64,9 +64,9 @@ subcategories = {
 
 categories = {
     'STEM': ['physics', 'chemistry', 'biology', 'computer science', 'math', 'engineering'],
-    'humanities': ['history', 'philosophy', 'law'],
-    'social sciences': ['politics', 'culture', 'economics', 'geography', 'psychology'],
-    'other (business, health, misc.)': ['other', 'business', 'health'],
+    'Humanities': ['history', 'philosophy', 'law'],
+    'Social Science': ['politics', 'culture', 'economics', 'geography', 'psychology'],
+    'Other': ['other', 'business', 'health'],
 }
 
 
