984a2e11716b232698b3a88302c9b881f3500e98
deling.sc <deling.sc@alibaba-inc.com>
2024-01-18 10:15:00 +0800

    feat: add TriviaQA support

---
+++ llmuses/benchmarks/trivia_qa/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from llmuses.benchmarks.trivia_qa.trivia_qa_adapter import TriviaQaAdapter, DATASET_ID, SUBSET_LIST
+from llmuses.benchmarks.trivia_qa.trivia_qa_adapter import TriviaQaAdapter as DataAdapterClass
+from llmuses.models.model_adapter import ChatGenerationModelAdapter as ModelAdapterClass    # noqa
