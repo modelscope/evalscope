31acafebd1707c4786c0a74ff85b697f3c19f186
deling.sc <deling.sc@alibaba-inc.com>
2024-02-28 17:29:06 +0800

    rename output folders.

--- llmuses/utils/utils.py
+++ llmuses/utils/utils.py
@@ -218,7 +218,7 @@ class ResponseParser:
         return ''
 
 
-def make_outputs_dir(work_dir: str, model_id: str, model_revision: str):
+def make_outputs_dir(work_dir: str, model_id: str, model_revision: str, dataset_id: str):
     model_revision = model_revision if model_revision is not None else 'none'
     # now = datetime.datetime.now()
     # format_time = now.strftime('%Y%m%d_%H%M%S')
