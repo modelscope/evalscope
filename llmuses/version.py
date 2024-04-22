9cbfb4761a2313c16149e75f548ef80547c69a44
Xingjun.Wang <wangxingjun778@163.com>
2024-04-22 15:28:28 +0800

    Merge main to release/0.3 (#38)
    * Fix cmmlu and support swift config (#36)
    
    * update
    
    * update cmmlu adapter
    
    * update version
    
    * fix cmmlu report
    
    * add norm score for cmmlu
    
    * Update generation template (#37)
    
    * update
    
    * update cmmlu adapter
    
    * update version
    
    * fix cmmlu report
    
    * add norm score for cmmlu
    
    * fix model type in run
    
    * add template type
    
    * add template type
    
    * update version
    
    * fix utils
    
    * update
    
    * update
    
    * add prompt_template for gsm8k
    
    * udpate version

--- llmuses/version.py
+++ llmuses/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.3.0'
-__release_datetime__ = '2024-04-11 20:00:00'
+__version__ = '0.3.1'
+__release_datetime__ = '2024-04-18 08:00:00'
