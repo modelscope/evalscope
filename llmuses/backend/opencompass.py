f34d22eeb610377503a31909e793de9c3544801b
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-05-31 14:18:09 +0800

    temp

--- llmuses/backend/opencompass.py
+++ llmuses/backend/opencompass.py
@@ -1,15 +1,28 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from dataclasses import dataclass, field, asdict
+from enum import Enum
 from typing import Optional, Union
 
 from llmuses.backend.base import BackendArgsParser
 
+
+class CmdMode(Enum):
+
+    # The basic mode is to run the command directly,
+    # e.g. `python -m run --models model1 model2 --datasets dataset1 dataset2`
+    BASIC = 'basic'
+
+    # The script mode is to run the command with a script,
+    # e.g. `python -m run your_config_script.py`
+    SCRIPT = 'script'
 
 @dataclass
 class OpenCompassArgs:
     models: Optional[list] = field(default_factory=list)
     datasets: Optional[list] = field(default_factory=list)
     dry_run: bool = False
+
+    script_file: Optional[str] = None
 
 
 class OpenCompassBackendArgsParser(BackendArgsParser):
@@ -21,6 +34,8 @@ class OpenCompassBackendArgsParser(BackendArgsParser):
         self.oc_args.models = self.config_d.get('models', [])
         self.oc_args.datasets = self.config_d.get('datasets', [])
         self.oc_args.dry_run = self.config_d.get('dry_run', False)
+        self.oc_args.script_file = self.config_d.get('script_file', None)
+
 
     @property
     def args(self):
@@ -37,18 +52,27 @@ class OpenCompassBackendArgsParser(BackendArgsParser):
         else:
             return ''
 
-    def get_cmd(self):
-        cmd_str = f'python3 -m run ' \
-                  f'--models {" ".join(self.oc_args.models)} ' \
-                  f'--datasets {" ".join(self.oc_args.datasets)} ' \
-                  f'{OpenCompassBackendArgsParser.get_restore_arg("--dry-run", self.oc_args.dry_run)}'
+    def get_cmd(self, cmd_mode: str = CmdMode.BASIC):
+
+        if cmd_mode == CmdMode.BASIC:
+            # TODO: 指定 --work-dir
+            cmd_str = f'python -m run ' \
+                      f'--models {" ".join(self.oc_args.models)} ' \
+                      f'--datasets {" ".join(self.oc_args.datasets)} ' \
+                      f'{OpenCompassBackendArgsParser.get_restore_arg("--dry-run", self.oc_args.dry_run)}'
+        elif cmd_mode == CmdMode.SCRIPT:
+            if self.oc_args.script_file is None:
+                raise ValueError('The script file is required in script mode.')
+            cmd_str = f'python -m run {self.oc_args.script_file}'
+        else:
+            raise ValueError(f'Unsupported command mode: {cmd_mode}')
 
         return cmd_str
 
 
 if __name__ == '__main__':
 
-    oc_task_cfg_file = '/temp/configs/qwen-1p5-7b-chat_mmlu_cfg.yaml'
+    oc_task_cfg_file = '../../examples/tasks/eval_qwen_oc_cfg.yaml'
     ocp = OpenCompassBackendArgsParser(config=oc_task_cfg_file)
     print(ocp.args)
     print(ocp.cmd)
