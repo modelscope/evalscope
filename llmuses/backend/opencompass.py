ae7540760f14f3733a6c5d176f1dffb0bf68450a
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-06-11 01:09:27 +0800

    update opencompass parser

--- llmuses/backend/opencompass.py
+++ llmuses/backend/opencompass.py
@@ -16,11 +16,13 @@ class CmdMode(Enum):
     # e.g. `python -m run your_config_script.py`
     SCRIPT = 'script'
 
+
 @dataclass
 class OpenCompassArgs:
     models: Optional[list] = field(default_factory=list)
     datasets: Optional[list] = field(default_factory=list)
     dry_run: bool = False
+    work_dir: Optional[str] = None
 
     script_file: Optional[str] = None
 
@@ -34,8 +36,9 @@ class OpenCompassBackendArgsParser(BackendArgsParser):
         self.oc_args.models = self.config_d.get('models', [])
         self.oc_args.datasets = self.config_d.get('datasets', [])
         self.oc_args.dry_run = self.config_d.get('dry_run', False)
-        self.oc_args.script_file = self.config_d.get('script_file', None)
+        self.oc_args.work_dir = self.config_d.get('work_dir', None)
 
+        self.oc_args.script_file = self.config_d.get('script_file', None)
 
     @property
     def args(self):
@@ -48,7 +51,14 @@ class OpenCompassBackendArgsParser(BackendArgsParser):
     @staticmethod
     def get_restore_arg(arg_name: str, arg_val: bool):
         if arg_val:
-            return arg_name
+            return f'--{arg_name}'
+        else:
+            return ''
+
+    @staticmethod
+    def get_arg_with_default(arg_name: str, arg_val: Optional[str] = None):
+        if arg_val:
+            return f'--{arg_name} {arg_val}'
         else:
             return ''
 
@@ -59,10 +69,13 @@ class OpenCompassBackendArgsParser(BackendArgsParser):
             cmd_str = f'python -m run ' \
                       f'--models {" ".join(self.oc_args.models)} ' \
                       f'--datasets {" ".join(self.oc_args.datasets)} ' \
-                      f'{OpenCompassBackendArgsParser.get_restore_arg("--dry-run", self.oc_args.dry_run)}'
+                      f'{OpenCompassBackendArgsParser.get_restore_arg("dry-run", self.oc_args.dry_run)} ' \
+                      f'{OpenCompassBackendArgsParser.get_arg_with_default("work-dir", self.oc_args.work_dir)}'
+
         elif cmd_mode == CmdMode.SCRIPT:
             if self.oc_args.script_file is None:
                 raise ValueError('The script file is required in script mode.')
+            # TODO: command `run` to be replaced with another name
             cmd_str = f'python -m run {self.oc_args.script_file}'
         else:
             raise ValueError(f'Unsupported command mode: {cmd_mode}')
@@ -75,5 +88,6 @@ if __name__ == '__main__':
     oc_task_cfg_file = '../../examples/tasks/eval_qwen_oc_cfg.yaml'
     ocp = OpenCompassBackendArgsParser(config=oc_task_cfg_file)
     print(ocp.args)
+    print()
     print(ocp.cmd)
 
