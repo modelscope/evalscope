cdcc7ebe2b275b66fc97782edbf94ec75eab9e7f
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-06-13 03:11:12 +0800

    refactor task config parser for toolbench

--- llmuses/third_party/toolbench_static/toolbench_static.py
+++ llmuses/third_party/toolbench_static/toolbench_static.py
@@ -1,5 +1,7 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+import os
 from typing import Union
+from copy import deepcopy
 
 from llmuses.third_party.toolbench_static.infer import InferArgs, run_infer
 from llmuses.third_party.toolbench_static.eval import EvalArgs, run_eval
@@ -13,16 +15,24 @@ def run_task(task_config: Union[str, dict]):
 
     # Run inference for each domain
     infer_args: dict = task_config['infer_args']
-    for domain, cfg_d in infer_args.items():
-        print(f'** Infer domain: {domain}, cfg: {cfg_d}')
-        task_infer_args = InferArgs(**cfg_d)
+    for domain in ['in_domain', 'out_of_domain']:
+        domain_infer_args = deepcopy(infer_args)
+        domain_infer_args.update({'data_path': os.path.join(infer_args['data_path'], f'{domain}.json')})
+        domain_infer_args.update({'output_dir': os.path.join(infer_args['output_dir'], domain)})
+
+        task_infer_args = InferArgs(**domain_infer_args)
+        print(f'**Run infer config: {task_infer_args}')
         run_infer(task_infer_args)
 
     # Run evaluation for each domain
     eval_args: dict = task_config['eval_args']
-    for domain, cfg_d in eval_args.items():
-        print(f'** Eval domain: {domain}, cfg: {cfg_d}')
-        task_eval_args = EvalArgs(**cfg_d)
+    for domain in ['in_domain', 'out_of_domain']:
+        domain_eval_args = deepcopy(eval_args)
+        domain_eval_args.update({'input_path': os.path.join(eval_args['input_path'], domain)})
+        domain_eval_args.update({'output_path': os.path.join(eval_args['output_path'], domain)})
+
+        task_eval_args = EvalArgs(**domain_eval_args)
+        print(f'**Run eval config: {task_eval_args}')
         run_eval(task_eval_args)
 
 
