3632e4db697eed93fa142f90f57590f70307d65b
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-07-11 11:15:10 +0800

    update version

--- llmuses/version.py
+++ llmuses/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.4.0'
-__release_datetime__ = '2024-06-27 08:00:00'
+__version__ = '0.4.1'
+__release_datetime__ = '2024-07-10 08:00:00'
