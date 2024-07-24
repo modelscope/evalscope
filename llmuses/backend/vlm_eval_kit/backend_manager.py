c7889f1cad84cf6d162e1b2e25f0532f187ba356
Yunnglin <maoyl@smail.nju.edu.cn>
2024-07-24 20:16:47 +0800

    add eval local model

--- llmuses/backend/vlm_eval_kit/backend_manager.py
+++ llmuses/backend/vlm_eval_kit/backend_manager.py
@@ -2,6 +2,7 @@ from typing import Optional, Union
 from llmuses.utils import is_module_installed, get_module_path, get_valid_list, yaml_to_dict, json_to_dict
 from llmuses.backend.base import BackendManager
 from llmuses.utils.logger import get_logger
+from functools import partial
 import subprocess
 from dataclasses import dataclass
 
@@ -28,8 +29,52 @@ class VLMEvalKitBackendManager(BackendManager):
         super().__init__(config, **kwargs)
 
         from vlmeval.utils.arguments import Arguments as VLMEvalArguments
+        print(self.config_d)
         self.args = VLMEvalArguments(**self.config_d)
         
+        self.valid_models = self.list_supported_VLMs()
+        self.valid_model_names = list(self.valid_models.keys())
+        self.valid_datasets = self.list_supported_datasets()
+        
+        self._check_valid()
+        print(self.args)
+        # process model path / tokenizer path
+    
+
+    def _check_valid(self):
+        # Ensure not both model and datasets are empty
+        if not self.args.data or not self.args.model:
+            raise ValueError('** Args: Please provide model and datasets. **')
+        
+        
+        # Check datasets
+        valid_datasets, invalid_datasets = get_valid_list(self.args.data, self.valid_datasets)
+        assert len(invalid_datasets) == 0, f'Invalid datasets: {invalid_datasets}, ' \
+                            f'refer to the following list to get proper dataset name: {self.valid_datasets}'
+        
+        # Check model
+        if isinstance(self.args.model[0], dict):
+            model_names = [model['name'] for model in self.args.model]
+            valid_model_names, invalid_model_names = get_valid_list(model_names, self.valid_model_names)
+            assert len(invalid_model_names) == 0, f'Invalid models: {invalid_model_names}, ' \
+                                f'refer to the following list to get proper model name: {self.valid_model_names}'
+            for model_cfg in self.args.model:
+                model_name = model_cfg['name']
+                if 'path' in model_cfg:
+                    model_class = self.valid_models[model_name]
+                    self.valid_models[model_name] = partial(model_class, model_path=model_cfg['path'])
+            
+                if 'tokenizer_path' in model_cfg:
+                    model_class = self.valid_models[model_name]
+                    self.valid_models[model_name] = partial(model_class, tokenizer_path=model_cfg['path'])
+            
+            self.args.model = valid_model_names
+                
+        elif isinstance(self.args.model[0], str):
+            valid_model_names, invalid_model_names = get_valid_list(self.args.model, self.valid_model_names)
+            assert len(invalid_model_names) == 0, f'Invalid models: {invalid_model_names}, ' \
+                                f'refer to the following list to get proper model name: {self.valid_model_names}'
+            
 
     @property
     def cmd(self):
@@ -38,7 +83,13 @@ class VLMEvalKitBackendManager(BackendManager):
     @staticmethod
     def list_supported_VLMs():
         from vlmeval.config import supported_VLM
-        return supported_VLM.keys()
+        return supported_VLM
+    
+    @staticmethod
+    def list_supported_datasets():
+        from vlmeval.dataset import SUPPORTED_DATASETS
+        return SUPPORTED_DATASETS
+    
 
     @staticmethod
     def _check_env():
@@ -84,11 +135,7 @@ class VLMEvalKitBackendManager(BackendManager):
         if run_mode == RunMode.CMD:
             logger.info(f'** Run command: {self.cmd}')
             try:
-                subprocess.run(self.cmd, 
-                               check=True, 
-                               text=True,
-                               shell=True,
-                               )
+                subprocess.run(self.cmd, check=True, ext=True,shell=True,)
             except subprocess.CalledProcessError as e:
                 logger.error(f'** Run command failed: {e.stderr}')
                 raise
