f278418045dd471bb6168a619c127cfb231dd646
Xingjun.Wang <wangxingjun778@163.com>
2024-08-29 18:34:10 +0800

    Add cmb (#117)
    * add cmb; fix import raise in eval_datasets
    
    * update req and eval_datasets imports
    
    * update
    
    * update
    
    * udpate
    
    * update example

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.5.2'
-__release_datetime__ = '2024-08-06 08:00:00'
+__version__ = '0.5.3'
+__release_datetime__ = '2024-08-29 08:00:00'
