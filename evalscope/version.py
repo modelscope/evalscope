dcc5346cd3544f285f8652f99cebbc00c1d197b6
Xingjun.Wang <wangxingjun778@163.com>
2024-09-29 14:18:48 +0800

    Add auto-download punkt_tab.zip for nltk (#140)
    * add longbench write
    
    * add readme and entry file
    
    * add package data for longbench resources
    
    * update model_id
    
    * update infer for test
    
    * update infer for test
    
    * update
    
    * add tqdm
    
    * udpate
    
    * fix replace
    
    * update
    
    * update
    
    * update process bar
    
    * add logger
    
    * update os.sep
    
    * fix cr comments
    
    * add openai api format for infer and eval
    
    * add openai_api_base for eval_config
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update transformers requirements
    
    * update
    
    * update
    
    * add wait()
    
    * update
    
    * update
    
    * update
    
    * del wait
    
    * update
    
    * add verbose
    
    * modify logs for generate
    
    * udpate
    
    * update log
    
    * update log
    
    * update
    
    * update
    
    * test
    
    * test
    
    * update
    
    * update
    
    * add mp
    
    * fix eval quality
    
    * fix eval quality
    
    * update
    
    * add repetition_penalty for infer config
    
    * update
    
    * add re-2 for test
    
    * update
    
    * add longwriter
    
    * add re-2
    
    * update
    
    * update
    
    * update
    
    * del re-2 code
    
    * update example
    
    * update exmaple and fix mbpp
    
    * update ms-oc req
    
    * update version to 0.5.5
    
    * update version
    
    * add punkt_zip auto-downloading

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.5.5rc0'
-__release_datetime__ = '2024-09-24 08:00:00'
+__version__ = '0.5.5rc1'
+__release_datetime__ = '2024-09-29 08:00:00'
