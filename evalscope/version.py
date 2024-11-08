147c315ab9f953392ba84bbc383e8be97f90e8b7
Xingjun.Wang <wangxingjun778@163.com>
2024-11-08 11:19:56 +0800

    update version (#185)
    * remove leaderboard in readme
    
    * set datasets version
    
    * update version

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = "0.5.5"
-__release_datetime__ = "2024-10-15 08:00:00"
+__version__ = "2.0.0"
+__release_datetime__ = "2099-01-01 00:00:00"
