fb2e0fd4d951adc0740b3683829ffcd34570540b
Yunlin Mao <mao.looper@qq.com>
2024-11-12 14:29:13 +0800

    fix #192 and compact mteb v1.19 (#196)

--- examples/example_eval_mteb.py
+++ examples/example_eval_mteb.py
@@ -17,84 +17,85 @@ def run_eval():
 
     # Prepare the config
 
-    one_stage_task_cfg = {
-        "eval_backend": "RAGEval",
-        "eval_config": {
-            "tool": "MTEB",
-            "model": [
-                {   
-                    "model_name_or_path": "AI-ModelScope/bge-large-zh",
-                    "pooling_mode": "cls",  # if not set, load from model config; use `cls` for bge series model
-                    "max_seq_length": 512,
-                    "prompt": "为这个句子生成表示以用于检索相关文章：",
-                    "encode_kwargs": {
-                        "batch_size": 512,
+    one_stage_task_cfg = {  # noqa
+        'eval_backend': 'RAGEval',
+        'eval_config': {
+            'tool': 'MTEB',
+            'model': [
+                {
+                    'model_name_or_path': 'AI-ModelScope/bge-large-zh',
+                    'pooling_mode': 'cls',  # if not set, load from model config; use `cls` for bge series model
+                    'max_seq_length': 512,
+                    'prompt': '为这个句子生成表示以用于检索相关文章：',
+                    'encode_kwargs': {
+                        'batch_size': 512,
                     },
                 }
             ],
-            "eval": {
-                "tasks": [
-                    "TNews",
-                    "CLSClusteringS2S",
-                    "T2Reranking",
-                    "ATEC",
-                    "T2Retrieval",
-                    "MMarcoRetrieval",
-                    "DuRetrieval",
-                    "CovidRetrieval",
-                    "CmedqaRetrieval",
-                    "EcomRetrieval",
-                    "MedicalRetrieval",
-                    "VideoRetrieval"
+            'eval': {
+                'tasks': [
+                    'TNews',
+                    'CLSClusteringS2S',
+                    'T2Reranking',
+                    'ATEC',
+                    'T2Retrieval',
+                    'MMarcoRetrieval',
+                    'DuRetrieval',
+                    'CovidRetrieval',
+                    'CmedqaRetrieval',
+                    'EcomRetrieval',
+                    'MedicalRetrieval',
+                    'VideoRetrieval'
                 ],
-                "verbosity": 2,
-                "output_folder": "outputs",
-                "overwrite_results": True,
-                "top_k" : 10,
-                "limits": 1000, # don't limit for retrieval task
+                'verbosity': 2,
+                'output_folder': 'outputs',
+                'overwrite_results': True,
+                'top_k': 10,
+                'limits': 1000,  # don't limit for retrieval task
             },
         },
     }
 
     two_stage_task_cfg = {
-        "eval_backend": "RAGEval",
-        "eval_config": {
-            "tool": "MTEB",
-            "model": [
+        'eval_backend': 'RAGEval',
+        'eval_config': {
+            'tool': 'MTEB',
+            'model': [
                 {
-                    "model_name_or_path": "AI-ModelScope/m3e-base",
-                    "is_cross_encoder": False,
-                    "max_seq_length": 512,
-                    "prompt": "",
-                    "model_kwargs": {"torch_dtype": "auto"},
-                    "encode_kwargs": {
-                        "batch_size": 64,
+                    'model_name_or_path': 'AI-ModelScope/m3e-base',
+                    'is_cross_encoder': False,
+                    'max_seq_length': 512,
+                    'prompt': '',
+                    'model_kwargs': {'torch_dtype': 'auto'},
+                    'encode_kwargs': {
+                        'batch_size': 64,
                     },
                 },
                 {
-                    "model_name_or_path": "OpenBMB/MiniCPM-Reranker",
-                    "is_cross_encoder": True,
-                    "max_seq_length": 512,
-                    "prompt": "为这个问题生成一个检索用的表示",
-                    "model_kwargs": {"torch_dtype": "auto"},
-                    "encode_kwargs": {
-                        "batch_size": 32,
+                    'model_name_or_path': 'OpenBMB/MiniCPM-Reranker',
+                    'is_cross_encoder': True,
+                    'max_seq_length': 512,
+                    'prompt': '为这个问题生成一个检索用的表示',
+                    'model_kwargs': {'torch_dtype': 'auto'},
+                    'encode_kwargs': {
+                        'batch_size': 32,
                     },
                 },
             ],
-            "eval": {
-                "tasks": ["T2Retrieval"],
-                "verbosity": 2,
-                "output_folder": "outputs",
-                "overwrite_results": True,
-                "limits": 100,
+            'eval': {
+                'tasks': ['T2Retrieval'],
+                'verbosity': 2,
+                'output_folder': 'outputs',
+                'overwrite_results': True,
+                'limits': 100,
             },
         },
     }
 
     # Run task
-    run_task(task_cfg=one_stage_task_cfg)
+    # run_task(task_cfg=one_stage_task_cfg)
+    run_task(task_cfg=two_stage_task_cfg)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_eval()
--- tests/rag/test_mteb.py
+++ tests/rag/test_mteb.py
@@ -12,7 +12,7 @@ logger = get_logger()
 class TestMTEB(unittest.TestCase):
 
     def setUp(self) -> None:
-        self._check_env("mteb")
+        self._check_env('mteb')
 
     def tearDown(self) -> None:
         pass
@@ -20,111 +20,111 @@ class TestMTEB(unittest.TestCase):
     @staticmethod
     def _check_env(module_name: str):
         if is_module_installed(module_name):
-            logger.info(f"{module_name} is installed.")
+            logger.info(f'{module_name} is installed.')
         else:
-            raise ModuleNotFoundError(f"run: pip install {module_name}")
+            raise ModuleNotFoundError(f'run: pip install {module_name}')
 
-    @unittest.skipUnless(0 in test_level_list(), "skip test in current test level")
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
     def test_run_one_stage_mteb(self):
         task_cfg = {
-            "eval_backend": "RAGEval",
-            "eval_config": {
-                "tool": "MTEB",
-                "model": [
+            'eval_backend': 'RAGEval',
+            'eval_config': {
+                'tool': 'MTEB',
+                'model': [
                     {
-                        "model_name_or_path": "AI-ModelScope/m3e-base",
-                        "pooling_mode": None,  # load from model config
-                        "max_seq_length": 512,
-                        "prompt": "",
-                        "model_kwargs": {"torch_dtype": "auto"},
-                        "encode_kwargs": {
-                            "batch_size": 128,
+                        'model_name_or_path': 'AI-ModelScope/m3e-base',
+                        'pooling_mode': None,  # load from model config
+                        'max_seq_length': 512,
+                        'prompt': '',
+                        'model_kwargs': {'torch_dtype': 'auto'},
+                        'encode_kwargs': {
+                            'batch_size': 128,
                         },
                     }
                 ],
-                "eval": {
-                    "tasks": [
-                        "TNews",
-                        "CLSClusteringS2S",
-                        "T2Reranking",
-                        "T2Retrieval",
-                        "ATEC",
+                'eval': {
+                    'tasks': [
+                        'TNews',
+                        'CLSClusteringS2S',
+                        'T2Reranking',
+                        'T2Retrieval',
+                        'ATEC',
                     ],
-                    "verbosity": 2,
-                    "output_folder": "outputs",
-                    "overwrite_results": True,
-                    "limits": 500,
+                    'verbosity': 2,
+                    'output_folder': 'outputs',
+                    'overwrite_results': True,
+                    'limits': 500,
                 },
             },
         }
 
         run_task(task_cfg)
 
-    @unittest.skipUnless(0 in test_level_list(), "skip test in current test level")
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
     def test_run_two_stage_mteb(self):
         task_cfg = {
-            "eval_backend": "RAGEval",
-            "eval_config": {
-                "tool": "MTEB",
-                "model": [
+            'eval_backend': 'RAGEval',
+            'eval_config': {
+                'tool': 'MTEB',
+                'model': [
                     {
-                        "model_name_or_path": "AI-ModelScope/m3e-base",
-                        "is_cross_encoder": False,
-                        "max_seq_length": 512,
-                        "prompt": "",
-                        "model_kwargs": {"torch_dtype": "auto"},
-                        "encode_kwargs": {
-                            "batch_size": 64,
+                        'model_name_or_path': 'AI-ModelScope/m3e-base',
+                        'is_cross_encoder': False,
+                        'max_seq_length': 512,
+                        'prompt': '',
+                        'model_kwargs': {'torch_dtype': 'auto'},
+                        'encode_kwargs': {
+                            'batch_size': 64,
                         },
                     },
                     {
-                        "model_name_or_path": "OpenBMB/MiniCPM-Reranker",
-                        "is_cross_encoder": True,
-                        "max_seq_length": 512,
-                        "prompt": "为这个问题生成一个检索用的表示",
-                        "model_kwargs": {"torch_dtype": "auto"},
-                        "encode_kwargs": {
-                            "batch_size": 32,
+                        'model_name_or_path': 'OpenBMB/MiniCPM-Reranker',
+                        'is_cross_encoder': True,
+                        'max_seq_length': 512,
+                        'prompt': '为这个问题生成一个检索用的表示',
+                        'model_kwargs': {'torch_dtype': 'auto'},
+                        'encode_kwargs': {
+                            'batch_size': 32,
                         },
                     },
                 ],
-                "eval": {
-                    "tasks": ["T2Retrieval"],
-                    "verbosity": 2,
-                    "output_folder": "outputs",
-                    "overwrite_results": True,
-                    "limits": 100,
+                'eval': {
+                    'tasks': ['MedicalRetrieval', 'T2Retrieval'],
+                    'verbosity': 2,
+                    'output_folder': 'outputs',
+                    'overwrite_results': True,
+                    'limits': 10,
                 },
             },
         }
 
         run_task(task_cfg)
 
-    @unittest.skipUnless(0 in test_level_list(), "skip test in current test level")
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
     def test_run_custom(self):
         task_cfg = {
-            "eval_backend": "RAGEval",
-            "eval_config": {
-                "tool": "MTEB",
-                "model": [
+            'eval_backend': 'RAGEval',
+            'eval_config': {
+                'tool': 'MTEB',
+                'model': [
                     {
-                        "model_name_or_path": "AI-ModelScope/m3e-base",
-                        "pooling_mode": None,  # load from model config
-                        "max_seq_length": 512,
-                        "prompt": "",
-                        "model_kwargs": {"torch_dtype": "auto"},
-                        "encode_kwargs": {
-                            "batch_size": 128,
+                        'model_name_or_path': 'AI-ModelScope/m3e-base',
+                        'pooling_mode': None,  # load from model config
+                        'max_seq_length': 512,
+                        'prompt': '',
+                        'model_kwargs': {'torch_dtype': 'auto'},
+                        'encode_kwargs': {
+                            'batch_size': 128,
                         },
                     }
                 ],
-                "eval": {
-                    "tasks": ["CustomRetrieval"],
-                    "dataset_path": "custom_eval/text/retrieval",
-                    "verbosity": 2,
-                    "output_folder": "outputs",
-                    "overwrite_results": True,
-                    "limits": 500,
+                'eval': {
+                    'tasks': ['CustomRetrieval'],
+                    'dataset_path': 'custom_eval/text/retrieval',
+                    'verbosity': 2,
+                    'output_folder': 'outputs',
+                    'overwrite_results': True,
+                    'limits': 500,
                 },
             },
         }
@@ -132,5 +132,5 @@ class TestMTEB(unittest.TestCase):
         run_task(task_cfg)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main(buffer=False)
