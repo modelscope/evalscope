de1a146c2033870563a19e8fd7ecb1a698b56d04
Yunlin Mao <mao.looper@qq.com>
2024-11-22 13:55:11 +0800

    compact ragas v0.2.5 and update readme (#205)
    * modify ragas
    
    * update readme
    
    * compact ragas v0.2.5

---
+++ evalscope/backend/rag_eval/ragas/prompts/persona_prompt.py
@@ -0,0 +1,18 @@
+import typing as t
+
+from pydantic import BaseModel
+from ragas.prompt import PydanticPrompt, StringIO
+from ragas.testset.persona import Persona
+
+
+class PersonaGenerationPromptZH(PydanticPrompt[StringIO, Persona]):
+    instruction: str = ('使用提供的摘要，生成一个可能会与内容互动或从中受益的角色。包括一个独特的名字和一个简洁的角色描述。')
+    input_model: t.Type[StringIO] = StringIO
+    output_model: t.Type[Persona] = Persona
+    examples: t.List[t.Tuple[StringIO, Persona]] = [(
+        StringIO(text='《数字营销指南》解释了在各种在线平台上吸引受众的策略。'),
+        Persona(
+            name='数字营销专家',
+            role_description='专注于吸引受众并在线上提升品牌。',
+        ),
+    )]
