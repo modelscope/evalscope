5e9c65cfe7ac4eb07033791dc40a3d5eb9089c1d
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-11-22 14:16:48 +0800

    update version to 0.6.1

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = "0.6.0"
-__release_datetime__ = "2024-11-08 11:59:59"
+__version__ = "0.6.1"
+__release_datetime__ = "2024-11-22 15:00:00"
