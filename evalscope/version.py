54eef6166df8f63e1d267deb5333b4e0fe883538
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-11-29 02:27:50 +0800

    add PMMEval benchmark

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = "0.7.0"
-__release_datetime__ = "2024-11-28 15:00:00"
+__version__ = "0.7.1"
+__release_datetime__ = "2024-11-29 03:00:00"
