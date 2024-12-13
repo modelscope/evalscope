657ea272c333671607ef6fd1c35da27156c102d5
Yunlin Mao <mao.looper@qq.com>
2024-12-13 16:16:11 +0800

    compat ragas 0.2.7 (#234)
    * compact ragas 0.2.7
    
    * compact ragas 0.2.7
    
    * update config
    
    * remove redundant

--- evalscope/backend/rag_eval/ragas/task_template.py
+++ evalscope/backend/rag_eval/ragas/task_template.py
@@ -1,7 +1,9 @@
-import os
 import asyncio
+import os
+
 from datasets import Dataset
-from evalscope.backend.rag_eval import EmbeddingModel, LLM
+
+from evalscope.backend.rag_eval import LLM, EmbeddingModel
 from evalscope.backend.rag_eval.ragas.tasks.translate_prompt import translate_prompts
 from evalscope.utils.logger import get_logger
 from .arguments import EvaluationArguments
@@ -9,9 +11,7 @@ from .arguments import EvaluationArguments
 logger = get_logger()
 
 
-def rag_eval(
-    args: EvaluationArguments,
-) -> None:
+def rag_eval(args: EvaluationArguments, ) -> None:
 
     from ragas import evaluate, RunConfig
     from ragas.llms import LangchainLLMWrapper
@@ -27,9 +27,6 @@ def rag_eval(
     llm = LLM.load(**args.critic_llm)
     embedding = EmbeddingModel.load(**args.embeddings)
 
-    # load dataset
-    dataset = Dataset.from_json(args.testset_file)
-
     # load metrics
     metrics = dynamic_import(*args.metrics)
     asyncio.run(
@@ -38,8 +35,9 @@ def rag_eval(
             target_lang=args.language,
             llm=LangchainLLMWrapper(llm),
             adapt_instruction=True,
-        )
-    )
+        ))
+    # load dataset
+    dataset = Dataset.from_json(args.testset_file)
 
     # evaluate
     runconfig = RunConfig(timeout=600, max_retries=2, max_wait=60, max_workers=1)
@@ -54,8 +52,6 @@ def rag_eval(
     logger.info(score_df)
 
     output_path = args.testset_file.replace('.json', '_score.json')
-    score_df.to_json(
-        output_path, indent=4, index=False, orient='records', force_ascii=False
-    )
+    score_df.to_json(output_path, indent=4, index=False, orient='records', force_ascii=False)
 
     logger.info(f'Eval score saved to {output_path}')
