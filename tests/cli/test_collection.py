1ea478c476ebb9c46129baa7813810d4a566bd53
Yunnglin <mao.looper@qq.com>
2024-12-24 18:43:44 +0800

    update test

--- tests/cli/test_collection.py
+++ tests/cli/test_collection.py
@@ -12,21 +12,19 @@ from evalscope.utils.utils import test_level_list
 class TestCollection(unittest.TestCase):
     @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
     def test_create_collection(self):
-        schema = CollectionSchema(
-            name='math&reasoning',
-            datasets=[
-                CollectionSchema(
-                    name='math',
-                    datasets=[
-                        DatasetInfo(name='gsm8k', weight=1, task_type='math', tags=['en', 'math']),
-                        DatasetInfo(name='competition_math', weight=2, task_type='math', tags=['en', 'math']),
+        schema = CollectionSchema(name='math&reasoning', datasets=[
+                    CollectionSchema(name='math', datasets=[
+                            DatasetInfo(name='gsm8k', weight=1, task_type='math', tags=['en', 'math']),
+                            DatasetInfo(name='competition_math', weight=1, task_type='math', tags=['en', 'math']),
+                            DatasetInfo(name='cmmlu', weight=2, task_type='math', tags=['zh', 'math'], args={'subset_list': ['college_mathematics', 'high_school_mathematics']}),
+                            DatasetInfo(name='ceval', weight=3, task_type='math', tags=['zh', 'math'], args={'subset_list': ['advanced_mathematics', 'high_school_mathematics', 'discrete_mathematics', 'middle_school_mathematics']}),
                     ]),
-                CollectionSchema(
-                    name='reasoning',
-                    datasets=[
-                        DatasetInfo(name='arc', weight=1, task_type='reasoning', tags=['en', 'reasoning']),
+                    CollectionSchema(name='reasoning', datasets=[
+                            DatasetInfo(name='arc', weight=1, task_type='reasoning', tags=['en', 'reasoning']),
+                            DatasetInfo(name='ceval', weight=1, task_type='reasoning', tags=['zh', 'reasoning'], args={'subset_list': ['logic']}),
+                            DatasetInfo(name='race', weight=1, task_type='reasoning', tags=['en', 'reasoning']),
                     ]),
-            ])
+                ])
         print(schema.to_dict())
         print(schema.flatten())
         schema.dump_json('outputs/schema_test.json')
@@ -36,7 +34,7 @@ class TestCollection(unittest.TestCase):
     def test_generate_data(self):
         schema = CollectionSchema.from_dict(json.load(open('outputs/schema_test.json', 'r')))
         print(schema.to_dict())
-        mixed_data = WeightedSampler(schema, 10).sample()
+        mixed_data = WeightedSampler(schema, 100).sample()
         dump_jsonl_data(mixed_data, 'outputs/mixed_data_test.jsonl')
 
     @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
