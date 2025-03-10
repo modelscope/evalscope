3487bda7511392fcbe32097561104a18d2202919
root <1576993271@qq.com>
2025-03-10 20:22:56 +0800

    v0.12.1

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.12.0'
-__release_datetime__ = '2025-02-27 21:00:00'
+__version__ = '0.12.1'
+__release_datetime__ = '2025-03-10 21:00:00'
