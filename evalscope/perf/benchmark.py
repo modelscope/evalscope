6730c40b6f0ee808a612a139c7c692297bba1020
Yunnglin <mao.looper@qq.com>
2025-04-07 11:18:06 +0800

    fix lint

--- evalscope/perf/benchmark.py
+++ evalscope/perf/benchmark.py
@@ -119,9 +119,7 @@ async def statistic_benchmark_metric_worker(benchmark_data_queue: asyncio.Queue,
         try:
             import wandb
         except ImportError:
-            raise RuntimeError(
-                "Cannot import wandb. Please install it with command: \n pip install wandb"
-            )
+            raise RuntimeError('Cannot import wandb. Please install it with command: \n pip install wandb')
         os.environ['WANDB_SILENT'] = 'true'
         os.environ['WANDB_DIR'] = args.outputs_dir
 
@@ -135,16 +133,17 @@ async def statistic_benchmark_metric_worker(benchmark_data_queue: asyncio.Queue,
         try:
             import swanlab
         except ImportError:
-            raise RuntimeError(
-                "Cannot import swanlab. Please install it with command: \n pip install swanlab"
-            )
+            raise RuntimeError('Cannot import swanlab. Please install it with command: \n pip install swanlab')
         os.environ['SWANLAB_LOG_DIR'] = args.outputs_dir
-        if not args.swanlab_api_key=="local":
+        if not args.swanlab_api_key == 'local':
             swanlab.login(api_key=args.swanlab_api_key)
         current_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
         name = args.name if args.name else f'{args.model_id}_{current_time}'
-        swanlab.init(project='perf_benchmark', name=name, config=args.to_dict(), mode="local" if args.swanlab_api_key=="local" else None)
-
+        swanlab.init(
+            project='perf_benchmark',
+            name=name,
+            config=args.to_dict(),
+            mode='local' if args.swanlab_api_key == 'local' else None)
 
     collected_benchmark_data = []
 
