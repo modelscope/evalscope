9015466b780abc7ed6c47d37ec6f920254fa54b7
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-04-30 11:45:58 +0800

    add v0.15.1

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.15.0'
-__release_datetime__ = '2025-04-29 00:00:00'
+__version__ = '0.15.1'
+__release_datetime__ = '2025-04-30 12:00:00'
