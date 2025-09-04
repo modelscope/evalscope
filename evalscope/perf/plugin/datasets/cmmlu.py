5aa930d5e9bf996c58e65796b40e3a32c50a2185
子悬 <hongkai.chk@alibaba-inc.com>
2025-09-04 20:05:56 +0800

    add cmmlu and gsm8k
    Signed-off-by: 子悬 <hongkai.chk@alibaba-inc.com>

--- evalscope/perf/plugin/datasets/cmmlu.py
+++ evalscope/perf/plugin/datasets/cmmlu.py
@@ -7,10 +7,10 @@ from evalscope.perf.plugin.datasets.base import DatasetPluginBase
 from evalscope.perf.plugin.registry import register_dataset
 
 
-@register_dataset('openqa')
-class OpenqaDatasetPlugin(DatasetPluginBase):
+@register_dataset('cmmlu')
+class CmmluDatasetPlugin(DatasetPluginBase):
     """Read dataset and return prompt.
-    Datasets: https://www.modelscope.cn/datasets/AI-ModelScope/HC3-Chinese/resolve/master/open_qa.jsonl
+    Datasets: https://huggingface.co/datasets/haonan-li/cmmlu/resolve/main/cmmlu-test.jsonl
     """
 
     def __init__(self, query_parameters: Arguments):
@@ -20,19 +20,25 @@ class OpenqaDatasetPlugin(DatasetPluginBase):
         if not self.query_parameters.dataset_path:
             from modelscope import dataset_snapshot_download
 
-            file_name = 'open_qa.jsonl'
-            local_path = dataset_snapshot_download('AI-ModelScope/HC3-Chinese')
+            file_name = 'cmmlu-test.jsonl'   # 实际文件名请按下载后的为准
+            local_path = dataset_snapshot_download(
+                'haonan-li/cmmlu',
+                allow_patterns=[file_name]
+            )
             self.query_parameters.dataset_path = os.path.join(local_path, file_name)
 
         for item in self.dataset_line_by_line(self.query_parameters.dataset_path):
             item = json.loads(item)
             prompt = item['question'].strip()
-            if (
-                len(prompt) > self.query_parameters.min_prompt_length
-                and len(prompt) < self.query_parameters.max_prompt_length
-            ):
-                if self.query_parameters.apply_chat_template:
-                    message = self.create_message(prompt)
-                    yield [message]
-                else:
-                    yield prompt
+
+            # 根据长度过滤
+            if not (self.query_parameters.min_prompt_length
+                    < len(prompt)
+                    < self.query_parameters.max_prompt_length):
+                continue
+
+            if self.query_parameters.apply_chat_template:
+                message = self.create_message(prompt)
+                yield [message]
+            else:
+                yield prompt
---
+++ evalscope/perf/plugin/datasets/gsm8k.py
@@ -0,0 +1,30 @@
+import json
+import os
+from typing import Any, Dict, Iterator, List
+
+from evalscope.perf.arguments import Arguments
+from evalscope.perf.plugin.datasets.base import DatasetPluginBase
+from evalscope.perf.plugin.registry import register_dataset
+
+
+@register_dataset('gsm8k')
+class Gsm8kDatasetPlugin(DatasetPluginBase):
+    """Read dataset and return prompt.
+    """
+
+    def __init__(self, query_parameters: Arguments):
+        super().__init__(query_parameters)
+
+    def build_messages(self) -> Iterator[List[Dict]]:
+        from modelscope.msdatasets import MsDataset
+        dataset =  MsDataset.load('modelscope/gsm8k', subset_name='main', split='train')
+
+        for item in dataset:
+            prompt = item['question'].strip()
+            if (len(prompt) > self.query_parameters.min_prompt_length
+                    and len(prompt) < self.query_parameters.max_prompt_length):
+                if self.query_parameters.apply_chat_template:
+                    message = self.create_message(prompt)
+                    yield [message]
+                else:
+                    yield prompt
