c984f3eb632a817b73bac3455587e7e37b6503be
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2025-10-28 07:46:06 +0000

    [Benchmark] Add Drivelology benchmark

---
+++ evalscope/benchmarks/drivelology/drivelology_selection_adapter.py
@@ -0,0 +1,45 @@
+from evalscope.api.benchmark import BenchmarkMeta, MultiChoiceAdapter
+from evalscope.api.dataset import Sample
+from evalscope.api.registry import register_benchmark
+from evalscope.constants import Tags
+
+DESCRIPTION = ''
+
+PROMPT_TEMPLATE = r"""
+Tell me the best option in the following options which represents the underlying narrative of the text?
+The entire content of your response should be of the following format: 'ANSWER: $LETTER' (without quotes) where LETTER is one of {letters}.
+
+{question}
+
+{choices}
+""".strip()  # noqa: E501
+
+
+@register_benchmark(
+    BenchmarkMeta(
+        name='drivel_selection',
+        pretty_name='DrivelologyNarrativeSelection',
+        tags=[Tags.MULTIPLE_CHOICE],
+        description=DESCRIPTION.strip(),
+        dataset_id='extraordinarylab/drivel-hub',
+        subset_list=['multiple-choice-english-easy', 'multiple-choice-english-hard'],
+        metric_list=['acc'],
+        few_shot_num=0,
+        train_split=None,
+        eval_split='test',
+        prompt_template=PROMPT_TEMPLATE,
+    )
+)
+class DrivelologyNarrativeSelectionAdapter(MultiChoiceAdapter):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_overall_metric = False
+
+    def record_to_sample(self, record) -> Sample:
+        return Sample(
+            input=record['text'],
+            choices=record['choices'],
+            target=record['answer'],
+            metadata={},
+        )
