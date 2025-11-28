ebe768ddd45905f1f3c85cae9d625bae662ee975
Yunnglin <mao.looper@qq.com>
2025-11-28 14:07:12 +0800

    update 1.3

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.0.0_dev'
-__release_datetime__ = '2099-01-01 00:00:00'
+__version__ = '1.3.0'
+__release_datetime__ = '2025-11-28 14:00:00'
