a6e4cfab867bdc909e483d7d8e7290e5260ae26b
Yunnglin <mao.looper@qq.com>
2025-11-28 15:47:44 +0800

    update

--- evalscope/benchmarks/arena_hard/utils.py
+++ evalscope/benchmarks/arena_hard/utils.py
@@ -172,7 +172,7 @@ def predict_win_rate(elo_ratings, SCALE=400, BASE=10, INIT_RATING=1000):
             wins[a][b] = ea
             wins[b][a] = 1 - ea
 
-    data = {a: [wins[a][b] if a != b else np.NAN for b in names] for a in names}
+    data = {a: [wins[a][b] if a != b else np.nan for b in names] for a in names}
 
     df = pd.DataFrame(data, index=names)
     df.index.name = 'model_a'
