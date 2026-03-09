063fbf90a149f1386759f9ad21ea12cd2f9ce195
Yunnglin <mao.looper@qq.com>
2026-03-09 18:47:01 +0800

    bump version 1.5.0

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.0.0_dev'
-__release_datetime__ = '2099-01-01 00:00:00'
+__version__ = '1.5.0'
+__release_datetime__ = '2026-03-09 19:00:00'
