20745554e5ed0a2d1a9842b2f8cf8304d77a4870
Yunnglin <mao.looper@qq.com>
2026-05-18 10:20:41 +0800

    update version 1.7.1

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '1.7.0'
-__release_datetime__ = '2026-05-07 12:00:00'
+__version__ = '1.7.1'
+__release_datetime__ = '2026-05-18 12:00:00'
