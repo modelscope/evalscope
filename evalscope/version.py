55736efd97c039d8f207baba9253083884d64220
Yunnglin <mao.looper@qq.com>
2026-08-24 15:54:40 +0800

    chore: release 1.11.0

--- evalscope/version.py
+++ evalscope/version.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
-__version__ = '0.0.0_dev'
-__release_datetime__ = '2099-01-01 00:00:00'
+__version__ = '1.11.0'
+__release_datetime__ = '2026-08-24 15:54:23'
